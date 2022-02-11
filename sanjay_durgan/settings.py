42e866fb8066900b5e0329baff11ee95e263b1de
Ishaan Nagpal <ishaannagpal@Ishaans-Air.Dlink>
2022-02-11 15:21:01 +0530

    Initial Commit

--- sanjay_durgan/settings.py
+++ sanjay_durgan/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-!65mo3k2ob1911fa1d-5n_x*%67$=qja2k2r&a^tc($di#@=2@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wwww.sanjaydurgan.herokuapp.com','sanjaydurgan.herokuapp.com']
 
 
 # Application definition
